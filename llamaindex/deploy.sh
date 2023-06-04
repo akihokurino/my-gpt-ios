0a9a53796637bfceafa685a98f61e88235307f8f
akihokurino <aki030402@gmail.com>
2023-06-05 00:59:32 +0900

    fixed

---
+++ llamaindex/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export VER=${VER:-local-$(date +%Y%m%d%H%M)}
+
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin $(aws sts get-caller-identity | jq -r .Account).dkr.ecr.ap-northeast-1.amazonaws.com
+docker build . -t 326914400610.dkr.ecr.ap-northeast-1.amazonaws.com/my-gpt:${VER}
+docker push 326914400610.dkr.ecr.ap-northeast-1.amazonaws.com/my-gpt:${VER}
+
+envsubst < fargate/api-definition.json > fargate/task-definition.json
+
+# aws ecs register-task-definition --cli-input-json file://$PWD/fargate/task-definition.json --profile me
