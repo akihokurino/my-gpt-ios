f47d32d4b71a39750509ed7816e93de42f2158e4
akihokurino <aki030402@gmail.com>
2023-06-04 23:26:59 +0900

    fixed

--- llamaindex/main.py
+++ llamaindex/main.py
@@ -162,5 +162,5 @@ def chat_completions():
 
 ############ Flaskサーバーの起動 ############
 if __name__ == "__main__":
-    print("Starting server... port=3000")
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    print(f"Starting server... port={os.getenv('PORT')}")
+    app.run(host="0.0.0.0", port=os.getenv("PORT"), debug=True)
